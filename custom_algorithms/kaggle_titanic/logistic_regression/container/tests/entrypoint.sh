d8eb0b2b2fb0d5f6f6da5e4406c381e34c43127d
ababa893 <flvonlineconverter@gmail.com>
2020-02-03 02:36:17 +0900

    Minor fix

--- custom_algorithms/kaggle_titanic/logistic_regression/container/tests/entrypoint.sh
+++ custom_algorithms/kaggle_titanic/logistic_regression/container/tests/entrypoint.sh
@@ -8,12 +8,14 @@
 
 # AWS CLI 用の環境変数を設定（docker run 時に，--envオプションで必要な環境変数が渡されていることを想定）
 # （CircleCI上でdocker run する場合は，予め管理画面にてAWS CLI 用環境変数をcontextまたはEnvironment Variablesに設定しておくこと．）
+echo "Create AWS CLI setting files"
 mkdir ~/.aws 
 echo -e "[default]\naws_access_key_id=$AWS_ACCESS_KEY_ID_DEV\naws_secret_access_key=$AWS_SECRET_ACCESS_KEY_DEV\n" > ~/.aws/credentials
 echo -e "[stg]\naws_access_key_id=$AWS_ACCESS_KEY_ID_STG\naws_secret_access_key=$AWS_SECRET_ACCESS_KEY_STG\n" >> ~/.aws/credentials
 echo -e "[prd]\naws_access_key_id=$AWS_ACCESS_KEY_ID_PRD\naws_secret_access_key=$AWS_SECRET_ACCESS_KEY_PRD\n" >> ~/.aws/credentials
 echo -e "[default]\nregion=$REGION\n[stg]\nregion=$REGION\n[prd]\nregion=$REGION\n" > ~/.aws/config
 
+echo "Prepare API local server"
 # SageMakerAPI用 スクリプトがあるディレクトリに移動
 cd /opt/program
 # trainスクリプトでS3から/opt/ml/modelディレクトリに推論用ファイルをDL
