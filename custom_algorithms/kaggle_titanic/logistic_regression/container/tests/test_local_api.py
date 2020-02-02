2142a97416b420d0539fe52c1f9d1ae4ac052afa
ababa893 <flvonlineconverter@gmail.com>
2020-02-03 06:10:11 +0900

    Add test codes and a CI job

---
+++ custom_algorithms/kaggle_titanic/logistic_regression/container/tests/test_local_api.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import urllib.request
+import json
+
+
+def test_api():
+    # テストするデータを用意する
+    name_test_json = 'data/test_input.json'
+    path_test_json = Path(__file__).parent.joinpath(name_test_json).resolve()
+    with open(path_test_json, 'r') as target:
+        test_data = json.load(target)
+
+    # シリアライズ
+    test_serialized = json.dumps(test_data).encode('utf-8')
+    print('リクエストBody: \n', test_data, '\n')
+
+    # データの送信
+    url = 'http://localhost:8080/invocations'
+    method = 'POST'
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    request = urllib.request.Request(url,
+                                     data=test_serialized,
+                                     headers=headers,
+                                     method=method)
+
+    # レスポンスを得る
+    with urllib.request.urlopen(request) as response:
+        payload = response.getheader('Content-Length')
+        result = response.read().decode('utf-8')
+        status_code = response.getcode()
+    result_dic = json.loads(result)
+
+    # 推論結果の表示
+    print('ステータスコード:', status_code)
+    print('レスポンスBody: \n', result_dic)
+    errmsg = f'異常終了 HTTP status: {status_code}\n エラー: \n{result_dic}'
+    assert status_code == 200, errmsg
+    print('Payload size: ', int(payload))
+
+
+if __name__ == "__main__":
+    test_api()
